d14a8e795fb7540fb196dd89e09839ae158a4ee4
Jesus Castro <castrojesus39@gmail.com>
2026-01-14 22:14:56 -0800

    included db connection string and added Sqlite via service

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,11 +1,19 @@
+using GameStore.Data;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Adds validation
 builder.Services.AddValidation();
+
+// Add DbContext via DI 
+var connString = "DataSource=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
