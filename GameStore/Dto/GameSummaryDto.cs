e3d89cb64054ba1713b723b9811d6118d3e050af
Jesus Castro <castrojesus39@gmail.com>
2026-03-25 22:21:02 -0700

    new dto for better understanding of Game

--- GameStore/Dto/GameSummaryDto.cs
+++ GameStore/Dto/GameSummaryDto.cs
@@ -2,10 +2,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace GameStore.Dto;
 
-public record GameDto(
+public record GameSummaryDto(
     int Id,
-    [Required][StringLength(50)] string Name,
-    [Required][StringLength(30)] string Genre,
+    [Required] [StringLength(50)] string Name,
+    [Required] [StringLength(30)] string Genre,
     decimal Price,
     DateOnly ReleaseDate
 );
